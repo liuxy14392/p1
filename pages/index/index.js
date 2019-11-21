029bca94f0b0b612fbedb84e5d5ca77dea0a5a5f
liuxy <1015907228@qq.com>
2019-11-21 14:47:45 +0800

    1.3

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,7 +21,7 @@ Page({
     nowTemp: '',
     nowWeather: '',
     nowWeatherBackground: "",
-    forecast : [1, 2, 3, 4,5,6,7],
+    forecast : [1, 2, 3, 4,5,6,7,8,9,10],
   },
   onPullDownRefresh() {
     //console.log("refresh executed!")
